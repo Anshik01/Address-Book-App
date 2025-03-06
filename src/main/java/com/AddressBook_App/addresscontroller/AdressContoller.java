f38ec0727428efa3df6d471b9bc6aa423fe05d1d
Anshik01 <anshik0706@gmail.com>
2025-03-06 16:40:43 +0530

    [Anshik] Add. demostrate different http request

--- src/main/java/com/AddressBook_App/addresscontroller/AdressContoller.java
+++ src/main/java/com/AddressBook_App/addresscontroller/AdressContoller.java
@@ -1,13 +1,36 @@
 package com.AddressBook_App.addresscontroller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/address")
 public class AdressContoller {
 
-    @GetMapping("/get")
-    public String getAddressBook() {
-        return "addressBook";
+    @GetMapping("/all")
+    public ResponseEntity<String> getAddressBook() {
+        return new ResponseEntity<>("address book", HttpStatus.OK);
     }
 
+    @GetMapping("/get/{id}")
+    public ResponseEntity<String> getAddressByID(@PathVariable long id){
+        return new ResponseEntity<>("address book", HttpStatus.OK);
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<String> postAddress(@RequestBody String address){
+        return new ResponseEntity<>("address book", HttpStatus.OK);
+    }
+
+    @PutMapping("/put/{id}")
+    public ResponseEntity<String> putAddress(@PathVariable long id,
+                                             @RequestBody String address){
+        return new ResponseEntity<>("address book", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> DeleteAddress(@PathVariable long id){
+        return new ResponseEntity<>("address book", HttpStatus.OK);
+    }
 }
